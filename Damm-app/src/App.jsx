218997d7dd8dd49c101ecbf5597db65142ba56b3
Martí Paulet Lòpez <78548539+martipaulet@users.noreply.github.com>
2024-05-01 12:25:26 +0200

    Form with navigation to Main Page

--- Damm-app/src/App.jsx
+++ Damm-app/src/App.jsx
@@ -1,12 +1,16 @@
-import { Form } from './components/Form'
+import { Form } from './components/Form/Form'
+import {Home} from './components/Home/Home'
+import {useState} from 'react'
 import './App.css'
 
 function App() {
-  
+
+  const [user, setUser] = useState("")
+
   return (
-    <div className= "App">
-        <Form/>
-    </div>
+    <div className="App">
+            {user ? <Home /> : <Form setUser={setUser} />}
+        </div>
   )
 }
 
